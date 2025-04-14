39af0a13a5eb222a80c78509b8285dfc248a468d
P@n0$ <panospapathanas@hotmail.com>
2025-04-14 18:51:36 +0300

    Addition of latest reads from different loggers.

--- hua_logger.py
+++ hua_logger.py
@@ -5,13 +5,28 @@ from modbus_reader import ModbusReader
 if __name__ == "__main__":
     
     # here we can accept a list of devices
-    device1 = ModbusDevice(ip="10.100.7.163", port=502, unit_id=100)
+    huawei = ModbusDevice(ip="10.100.7.163", port=502, unit_id=100)
+    sunGrow = ModbusDevice(ip="10.100.2.163", port=502, unit_id=247)
+    froniusGen24 = ModbusDevice(ip="10.108.1.51", port=502, unit_id=1)
+    froniusDatamanager = ModbusDevice(ip="10.100.2.161", port=502, unit_id=1)
 
     # here we can accept a list of registers 
-    registers = [
+    huaRegisters = [
         Register(address=40543, name="Plant Status", scale=1, unit="")
     ]
 
+    sungrowRegister = [
+        Register(address=8001, name="Plant Status", scale=1, unit="")
+    ]
+
+    froniusGen24Register = [
+        Register(address=40242, name="Plant Status", scale=1, unit="")
+    ]
+
+    froniusDatamanagerRegister = [
+        Register(address=40242, name="Plant Status", scale=1, unit="")
+    ]
+    
     # creating a ModbusReader object to read all the provided registers
-    reader = ModbusReader(device=device1, registers=registers)
+    reader = ModbusReader(device=froniusDatamanager, registers=froniusDatamanagerRegister)
     reader.read_all_registers()
